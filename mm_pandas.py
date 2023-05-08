c667e42bac0ff9599be9ec423f35d8d8605e1567
rawlemaraj <rawlemaraj@hotmail.com>
2023-05-08 14:34:07 -0400

    2nd

--- mm_pandas.py
+++ mm_pandas.py
@@ -1,37 +1,28 @@
-import os
+
 import pandas as pd
 from docx import Document
 
-# Read Excel data
+# Read the Excel data
 excel_file = 'data.xlsx'
-data = pd.read_excel(excel_file)
+data = pd.read_excel(excel_file, engine='openpyxl')
 
-# Load Word template
+# Load the Word template
 template_file = 'template.docx'
 
-# Output directory for generated documents
-output_dir = 'output'
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
-
-def replace_placeholders(paragraph, record):
-    """Replace placeholders in a paragraph with values from a record."""
-    for key, value in record.items():
-        placeholder = f'{{{{{key}}}}}'
-        if placeholder in paragraph.text:
-            paragraph.text = paragraph.text.replace(placeholder, str(value))
-
-# Perform mail merge
+# Iterate over each row in the Excel data
 for index, row in data.iterrows():
-    # Load the Word template for each record
+    # Create a new document from the template
     doc = Document(template_file)
 
-    # Replace placeholders in the document with data from the current row
+    # Replace placeholders in the document with data from the Excel file
     for paragraph in doc.paragraphs:
-        replace_placeholders(paragraph, row)
+        for key in row.keys():
+            placeholder = f'<<{key}>>'
+            if placeholder in paragraph.text:
+                paragraph.text = paragraph.text.replace(placeholder, str(row[key]))
 
-    # Save the merged document in the output directory
-    output_file = os.path.join(output_dir, f'merged_document_{index+1}.docx')
+    # Save the new document
+    output_file = f'output_{index + 1}.docx'
     doc.save(output_file)
 
-print('Mail merge completed.')
+print("Mail merge complete.")
